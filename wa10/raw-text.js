833f072d3f60f3eef3c5755df6087f7009434a73
Jake <jado1878@colorado.edu>
2025-03-25 20:08:58 -0600

    wa10

---
+++ wa10/raw-text.js
@@ -0,0 +1,42 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+const storyText = 'They really needed to do their homework, so :insertx: made a cup of hot coffee. They sat down at their desk and pulled out their :inserty:. They began on their :insertz: homework. Hours passed, but finally, after an absurd amount of caffeine was consumed, they finished.';
+const insertX = ['Jake', 'Dave', 'Sue'];
+const insertY = ['notebook', 'textbook', 'calculator'];
+const insertZ = ['english', 'science', 'algebra'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(':insertx:',xItem);
+  newStory = newStory.replaceAll(':inserty:',yItem);
+  newStory = newStory.replaceAll(':insertz:',zItem);
+
+  if (customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replaceAll('Bob', name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory = newStory.replaceAll('94 fahrenheit', temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
